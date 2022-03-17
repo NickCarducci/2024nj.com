53380f952a9118fb6b14e381136c80b534d03999
Nick Carducci <nmcarducci@gmail.com>
2022-03-17 17:48:14 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -333,6 +333,7 @@ export default class App extends React.Component {
         {posts.map((p) => {
           return <div>{p.message}</div>;
         })}
+        <h4>Taxing large accounts is the most taxing</h4>
         You don't need to medicate to house, for trespass is not adjunicatable,
         but you can't make this a solution when rental-income is
         laborless-demand of implausible use leases, general fund and closed
