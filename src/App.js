ce7cca918a61f79fde88f66a939d4d8a50341e14
Nick Carducci <nmcarducci@gmail.com>
2022-04-27 01:54:17 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -585,6 +585,7 @@ export default class App extends React.Component {
             </a>
           </div>
         </div>
+        Are you willing to pay?<br/>
         Transitory won't make unemployment.<br/>
         Money in pockets, people in jobs, resources in order to move forward
         <br />
