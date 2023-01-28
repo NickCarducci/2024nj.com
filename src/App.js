9771b2f8e5c62074b47c0c704567169e29e36359
Nick Carducci <nmcarducci@gmail.com>
2023-01-28 13:25:33 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -536,7 +536,10 @@ export default class App extends React.Component {
               />
             </a>
           </div>
-        </div>psychitric medicine doesn't change the alleged crime of the following:
+        </div>
+        
+        is it a war crime to fight for the right to own?
+        psychitric medicine doesn't change the alleged crime of the following:
         if wall street keeps causing inflation I will rip their throats out.<br/>
         SELF DEFENSE. IF FLOYD CAN BE CARFACED YOU CAN TOO<br/>
         <h3 style={{ float: "right" }}>
